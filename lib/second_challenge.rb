00cadf49848812beb1fafc67bf07f24803c9ce0e
Kyle Stewart <kyle.w.stewart@mac.com>
2017-03-23 18:32:12 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,8 +6,17 @@ def second_challenge
    grains: ["rice", "pasta"]
   }
 
-  #code your solution here!
+  arrays = groceries.values
 
-  
+  flat_array = []
+  arrays.each do |array|
+    i = 0
+    while i < array.length
+      flat_array << array[i]
+      i += 1
+    end
+  end
+
+  flat_array
 
 end
